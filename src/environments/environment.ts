6c44ed06cf360662d7684956fa784e848dc95b6b
crosasv <crosasv@gmail.com>
2020-07-29 12:29:18 -0400

    asd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiUrl: 'http://localhost:59510/',//'https://siga.desa.inacap.cl/Inacap.siga.CtaCteT1T2/', //,
+  apiUrl: 'https://siga.desa.inacap.cl/Inacap.siga.FUP_Circ42/', //,
   production: false
 };
 
